2dbca30904a1e253bd8933f60410fa8bf21bbe6b
RaST-EDITH <raghvsinghtomar1975@gmail.com>
2024-01-01 21:45:27 +0530

    Construction function added

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ import sys
 import os
 
 os.environ["OPENAI_API_KEY"] = ''
+
+def construct_index(directory_path):
+    max_input_size = 4096
+    num_outputs = 512
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
