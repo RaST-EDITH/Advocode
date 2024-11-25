c00d0c0b7c71f57c8636093b3eb9127b88c7e4a2
Raghvendra Singh <raghvsinghtomar1975@gmail.com>
2024-11-25 10:29:16 +0530

    message js added

---
+++ Frontend/src/components/chat/components/messages.jsx
@@ -0,0 +1,31 @@
+const Messages = () => {
+  return (
+    <section id="messages">
+      <div id="examples" className="max-w-md">
+        <p className="text-[#6f6f6f] text-lg font-semibold pb-6 w-full text-center">
+          Type something like:
+        </p>
+        <div className="flex flex-col gap-3 text-xs sm:text-sm font-light text-[#737373] font-poppins">
+          <p className="bg-[#F5F5F5] rounded-sm p-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            ullam fuga rem veritatis consequuntur culpa placeat id quas,
+          </p>
+          <p className="bg-[#F5F5F5] rounded-sm p-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            ullam fuga rem veritatis consequuntur culpa placeat id quas,
+          </p>
+          <p className="bg-[#F5F5F5] rounded-sm p-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            ullam fuga rem veritatis consequuntur culpa placeat id quas,
+          </p>
+          <p className="bg-[#F5F5F5] rounded-sm p-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            ullam fuga rem veritatis consequuntur culpa placeat id quas,
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Messages;
